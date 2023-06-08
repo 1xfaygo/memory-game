a84f8c100d218be384571e77df14a060a8b75b71
1xfaygo <113103007+1xfaygo@users.noreply.github.com>
2023-06-08 11:22:49 +0200

    '11:30'

--- game/index.js
+++ game/index.js
@@ -1,5 +1,80 @@
 var foto = document.createElement('img');
 foto.src = 'foto/usa.png';
-foto.style.width = '10%';
+foto.style.width = '100px';
+foto.style.height = '100px';
+foto.style.margin = '10px';
 var src = document.getElementById('spel');
 src.appendChild(foto);
+
+var foto2 = document.createElement('img');
+foto2.src = 'foto/UK.png';
+foto2.style.width = '100px';
+foto2.style.height = '100px';
+foto2.style.margin = '10px';
+var src = document.getElementById('spel');
+src.appendChild(foto2); 
+
+var foto3 = document.createElement('img');
+foto3.src = 'foto/japan.png';
+foto3.style.width = '100px';
+foto3.style.height = '100px';
+foto3.style.margin = '10px';
+var src = document.getElementById('spel');
+src.appendChild(foto3); 
+
+var foto4 = document.createElement('img');
+foto4.src = 'foto/nederland.png';
+foto4.style.width = '100px';
+foto4.style.height = '100px';
+foto4.style.margin = '10px';
+var src = document.getElementById('spel');
+src.appendChild(foto4);
+
+var foto5 = document.createElement('img');
+foto5.src = 'foto/france.png';
+foto5.style.width = '100px';
+foto5.style.height = '100px';
+foto5.style.margin = '10px';
+var src = document.getElementById('spel'); 
+src.appendChild(foto5); 
+
+var foto6 = document.createElement('img');
+foto6.src = 'foto/zweden.png';
+foto6.style.width = '100px';
+foto6.style.margin = '10px';
+foto6.style.height = '100px';
+var src = document.getElementById('spel');
+src.appendChild(foto6);
+
+var foto7 = document.createElement('img');
+foto7.src = 'foto/duitsland.png';
+foto7.style.width = '100px';
+foto7.style.height = '100px';
+foto7.style.margin = '10px';
+var src = document.getElementById('spel');
+src.appendChild(foto7); 
+
+var foto8 = document.createElement('img');
+foto8.src = 'foto/canada.png';
+foto8.style.width = '100px';
+foto8.style.margin = '10px';
+foto8.style.height = '100px';
+var src = document.getElementById('spel');
+src.appendChild(foto8);
+
+var foto9 = document.createElement('img');
+foto9.src = 'foto/australia.png';
+foto9.style.width = '100px';
+foto9.style.height = '100px';
+foto9.style.margin = '10px';
+var src = document.getElementById('spel'); 
+src.appendChild(foto9); 
+
+var foto10 = document.createElement('img');
+foto10.src = 'foto/denmark.png';
+foto10.style.width = '100px';
+foto10.style.height = '100px';
+foto10.style.margin = '10px';
+var src = document.getElementById('spel');
+src.appendChild(foto10); 
+
