908f821e4af2d8414b5a4a7d4f35f5404b134809
1xfaygo <113103007+1xfaygo@users.noreply.github.com>
2023-06-08 10:22:13 +0200

    '10:30'

--- game/index.js
+++ game/index.js
@@ -1,5 +1,5 @@
-function clickfoto(){
-    var foto = document.createElement('img')
-    foto.src = 'foto/usa.png'
-    document.querySelector('.container').appendChild(foto)
-}
+var foto = document.createElement('img');
+foto.src = 'foto/usa.png';
+foto.style.width = '10%';
+var src = document.getElementById('spel');
+src.appendChild(foto);
