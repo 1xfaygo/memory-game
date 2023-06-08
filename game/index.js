e0496dfce38799439ef48cf49bd354a8c68a5dcc
1xfaygo <113103007+1xfaygo@users.noreply.github.com>
2023-06-08 15:50:25 +0200

    '15:50'

--- game/index.js
+++ game/index.js
@@ -1,4 +1,6 @@
+
 var pictures =[
+    'foto/aarde.jpg',
     'foto/UK.png',
     'foto/usa.png',
     'foto/france.png',
@@ -11,14 +13,19 @@ var pictures =[
     'foto/duitsland.png',
 ]
     var foto = document.createElement('div');
-    for(x = 0; x < 2; x ++){
-        for(var x = 0; x < pictures.length; x++){
+    for(i = 0; i < 2; i ++){
+        for(var x = 0; x < pictures.length -1 ; x++){
             var images = document.createElement('img');
+
             images.style.width = '100px';
             images.style.height = '100px';
-            images.style.margin = '10px';   
-            images.src = pictures[x];
+            images.style.margin = '10px';
+            images.setAttribute('onclick','change()');
+            images.src = pictures[0];
             src = document.getElementById('spel');
             src.appendChild(images);
         } 
 }
+function change(){
+    
+} 
